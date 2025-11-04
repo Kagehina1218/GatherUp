a060a8df3963ae2b037395eb5cecff78d9faaa20
KaiCoder1 <131473153+KaiCoder1@users.noreply.github.com>
2025-11-04 14:33:49 -0800

    signup working now + with navbar frontend

--- my-flask-app/app.py
+++ my-flask-app/app.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, request, jsonify, session, url_for
+from flask import Flask, render_template, request, jsonify, session, url_for, redirect
 
 import json
-from db_utils import create_user
+from db_utils import create_user, check_user
 from gemini_utils import generate_schedule
 
 
@@ -10,11 +10,11 @@ app.secret_key = "secretKey222"
 
 @app.route("/", methods = ["GET"])
 def home():
-    username = "kain"
-    password = "johnson"
+    # username = "kain"
+    # password = "johnson"
 
-    result = create_user(username, password)
-    print(result)
+    # result = create_user(username, password)
+    # print(result)
 
     return render_template("login.html")
 
@@ -24,20 +24,53 @@ def demo():
 
     return render_template("demo.html", text = responseText or "")
 
+
 @app.route("/loginhere", methods = ["POST"])
 def login():
     try:
         username = request.form.get('username')
         password = request.form.get('password')
 
-        result = create_user(username, password)
+        result = check_user(username, password)
 
         print (result)
+
+        if result:
+            return redirect(url_for('demo'))
+        
 
         
     except Exception as e:
         print(e)
-    return render_template("login.html")
+
+    return redirect(url_for('home'))
+
+
+
+
+@app.route("/signup", methods = ["POST", "GET"])
+def signup():
+    print("inside signup")
+    
+    if request.method == 'POST':
+        try:
+            username = request.form.get('username')
+            password = request.form.get('password')
+
+            result = check_user(username, password)
+
+            print("inside post--", result)
+
+            if not result:
+                result = create_user(username, password)
+                print(result)
+                return redirect(url_for('demo'))
+            
+        except Exception as e:
+            print(e)
+    
+    return render_template('signup.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- my-flask-app/db_utils.py
+++ my-flask-app/db_utils.py
@@ -8,11 +8,19 @@ User = Query()
 def create_user(username, password):
     existing = users_table.get(User.username == username)
     if existing:
-        return {"message": "Username already exists", "status": "error"}
+        return {"message": "user already exists!", "status": "error"}
 
     users_table.insert({"username": username, "password": password, "schedule": []})
     return {"message": "Signup successful!", "status": "success"}
 
+
+def check_user(username, password):
+    existing = users_table.get((User.username == username) & (User.password == password))
+    if existing:
+        print("Message: username/password found")
+        return True
+    print("Message: Username/password not found")
+    return False
 
 def update_schedule(username, schedule):
     existing = users_table.get(User.username == username)
